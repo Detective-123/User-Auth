fba07306648c8c703efd9f04bd1ca5b31cc69e9e
Detective-123 <gauravsawant8217@gmail.com>
2025-09-17 20:24:46 +0530

    user controller updated

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,12 +1,25 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, updateAccountDetails } from "../controllers/user.controllers.js";
+import { 
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    updateAccountDetails, 
+    changeCurrentPassword, 
+    refreshAccessToken, 
+    getCurrentUser
+} from "../controllers/user.controllers.js";
 
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
 // secured routes
-// router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").post(verifyJWT, updateAccountDetails);
 
 export default router
