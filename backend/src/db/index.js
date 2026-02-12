0bf1cf32037576a15e93a425bf611e5c96cafe0e
Gaurav Sawant <gauravsawant8217@gmail.com>
2026-02-12 18:17:33 +0530

    organised the project

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -3,11 +3,11 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
 
-        console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+        console.log(`MongoDB connected successfully ✅`);
     } catch (error) {
-        console.log("MongoDB connection errorr: ", error);
+        console.log("MongoDB connection errorr ❌ ", error);
         process.exit(1);
     }
 }
